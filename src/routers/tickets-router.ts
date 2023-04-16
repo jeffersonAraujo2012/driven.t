a78f6d9fde9efe8edb4f3560e65923a4064b80ed
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-04-15 22:08:06 -0300

    Update: useless code removed.

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -6,10 +6,6 @@ import { createTicketSchema } from '@/schemas/tickets-schema';
 
 const ticketsRouter = Router();
 
-ticketsRouter.get('/token', async (req, res) => {
-  console.log(await generateValidToken());
-  res.sendStatus(204);
-});
 ticketsRouter.all('/*', authenticateToken);
 ticketsRouter.get('/', getUserTicket);
 ticketsRouter.get('/types', getTicketTypes);
